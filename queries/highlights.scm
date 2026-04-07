a53384afc291037ed19f349f18c2792933ca64db
Fadarrizz <aukegeerts@duck.com>
2026-04-07 12:14:16 +0200

    Add highlights query

---
+++ queries/highlights.scm
@@ -0,0 +1,20 @@
+(date) @type.definition
+
+(should_total) @type
+
+(time) @function
+
+(time_association) @operator
+
+(open_range) @comment
+
+(positive_duration) @number
+
+(negative_duration) @number
+
+; Tags
+"#" @punctuation.special
+(tag_name) @keyword
+(tag_value) @string
+
+(summary) @string
